53ef858d5ace32aee5cd6453bbf12ee0af295313
unlimited <danya.polyakov.05@gmail.com>
2026-03-29 19:45:22 +0300

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,11 @@
+class MinStack:
+    def __init__(self):
+        self._stack = []
+    def push(self, val: int) -> None:
+        self._stack.append(val)
+    def pop(self) -> None:
+        self._stack.pop()
+    def top(self) -> int:
+        return self._stack[-1]
+    def getMin(self) -> int:
+        return min(self._stack)

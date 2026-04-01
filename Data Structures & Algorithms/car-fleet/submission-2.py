6e64ac49ba2e21a37780875cbe18cc92913daa42
unlimited <danya.polyakov.05@gmail.com>
2026-04-01 18:04:42 +0300

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        res = 0
+        stack = []
+        race = [tuple([0, 0]) for i in range(target + 1)]
+        for i in range(len(position)):
+            race[position[i]] = tuple([position[i], speed[i]])
+        for i in range(target, -1, -1):
+            if race[i] == (0, 0):
+                continue
+            if stack != []:
+                t1 = (target - race[i][0]) / race[i][1]
+                t2 = (target - stack[0][0]) / stack[0][1]
+                if t1 > t2:
+                    while stack != []:
+                        stack.pop()
+                    res += 1
+                stack.append(race[i])
+            else:
+                stack.append(race[i])
+        return res + 1
+
+        """ решение нормальное, по сложности O(target + N) (target > N обычно) и O(target)
+        есть вариант лучше - не делать огромный список race а просто нормально отсортировать 
+        по позициям, и получится память лишь O(N) и время O(NlogN)
+        """

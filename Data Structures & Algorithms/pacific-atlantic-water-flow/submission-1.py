2c83ce4dd59a7bdeb71927efaf14824148ff363a
unlimited <danya.polyakov.05@gmail.com>
2026-06-15 14:56:16 +0300

    Add: pacific-atlantic-water-flow - submission-1

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        pac, atl = set(), set()
+        n, m = len(heights), len(heights[0])
+
+        def dfs(i, j, cur, visited):
+            if (i, j) in visited:
+                return
+
+            cur.add((i, j))
+            visited.add((i, j))
+            if i - 1 >= 0:
+                if heights[i - 1][j] >= heights[i][j]: dfs(i - 1, j, cur, visited)
+            if i + 1 < n:
+                if heights[i + 1][j] >= heights[i][j]: dfs(i + 1, j, cur, visited)
+            if j - 1 >= 0:
+                if heights[i][j - 1] >= heights[i][j]: dfs(i, j - 1, cur, visited)
+            if j + 1 < m:
+                if heights[i][j + 1] >= heights[i][j]: dfs(i, j + 1, cur, visited)
+
+        for j in range(m):
+            dfs(0, j, pac, set())
+            dfs(n - 1, j, atl, set())
+        for i in range(n):
+            dfs(i, 0, pac, set())
+            dfs(i, m - 1, atl, set())
+
+        res = []
+        for i in range(n):
+            for j in range(m):
+                if (i, j) in pac and (i, j) in atl:
+                    res.append([i, j])
+
+        return res
